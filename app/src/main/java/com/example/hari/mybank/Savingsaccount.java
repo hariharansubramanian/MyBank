c7f01227aa7d44b76dcb7085bfa26027caa15085
hariharansubramanian <hariharan1990.s@gmail.com>
2015-07-24 18:21:04 +0530

    added savings account logic..

--- app/src/main/java/com/example/hari/mybank/Savingsaccount.java
+++ app/src/main/java/com/example/hari/mybank/Savingsaccount.java
@@ -1,8 +1,18 @@
 package com.example.hari.mybank;
 
+import android.util.Log;
+
 /**
  * Created by Nicky on 7/24/2015.
  */
 public class Savingsaccount extends BankAccount {
-
+    public static final String TAG="Savingsaccount";
+    @Override
+    public void Withdraw(double amount) {
+        Log.d(TAG,"Withdrawing from Savings..");
+        if(numberOfNegativeWithdrawals()>=3){
+            return;
+        }
+        super.Withdraw(amount);
+    }
 }
